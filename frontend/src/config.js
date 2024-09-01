624b7290ac85ce5a019177877e827d5229fe785e
Sagar <sagarbasak2020@gmail.com>
2024-09-01 20:18:33 +0530

    ADDED new BASE_URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,5 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+//export const BASE_URL = "http://localhost:5000/api/v1";
+
+export const BASE_URL = "https://medicare-backend-qd6a.onrender.com/api/v1";
+
 export const token = localStorage.getItem("token");
