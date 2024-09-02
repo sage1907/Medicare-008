43dd84d7c86a68e005f638375b700ac6e00f87fc
Sagar <sagarbasak2020@gmail.com>
2024-09-02 20:40:31 +0530

    minor updates regarding cors

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-
 // route imports
 import authRoutes from "./Routes/auth.js";
 import userRoutes from "./Routes/user.js";
@@ -18,53 +17,48 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-const allowedOrigins = ['https://medicare-sage-six.vercel.app/'];
-
-// const corsOptions = {
-//     origin: true
-// };
+const allowedOrigins = ['https://medicare-sage-six.vercel.app'];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true, // if you need to send cookies or other credentials
+  credentials: true,
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
 };
 
-
-
-app.get('/', (req, res) => {
-    res.send("Api is working!");
-});
-
-// database connection
-mongoose.set('strictQuery', false);
-const connectDB = async() => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-
-        console.log("MongoDB connection established.");
-    } catch (error) {
-        console.log("MongoDB connection failed.");
-    }
-}
+app.use(cors(corsOptions)); // Apply CORS before all routes
 
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
 
 // routes
+app.get('/', (req, res) => {
+    res.send("Api is working!");
+});
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/doctors", doctorRoutes);
 app.use("/api/v1/doctors/:doctorId/reviews", reviewRoutes);
 app.use("/api/v1/bookings", bookingRoutes);
 app.use("/api/v1/feedback", feedbackRoutes);
+
+// database connection
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDB connection established.");
+    } catch (error) {
+        console.log("MongoDB connection failed.");
+    }
+};
 
 app.listen(port, () => {
     connectDB();
